7bdc1a1e66dd52d0ef0c6663f015f9c8053e5852
Glen Egbert <glenegbert1@gmail.com>
2018-12-12 13:14:18 -0700

    same priority assignment works

--- app/models/zone_ad_forecast.rb
+++ app/models/zone_ad_forecast.rb
@@ -2,19 +2,41 @@ module ZoneAdForecast
   def self.forecast(zone_id, date)
     zone = Zone.find(zone_id)
     available_impressions = zone.impressions.to_f
-    ads(zone, date).each_with_object([]) do |ad, accum|
-      add_to_report(available_impressions, ad, accum)
-      next_impressions = available_impressions - daily_goal(ad)
+    ads = ads(zone,date).group_by(&:priority)
+    ads.values.each_with_object([]) do |ads, accum|
+      daily_assigned = ads_to_report(available_impressions, ads, accum)
+      next_impressions = available_impressions - daily_assigned
       # keep available impressions from being negative so next calculations work
       available_impressions = next_impressions.positive? ? next_impressions : 0
     end
   end
 
   class << self
-    def add_to_report(available_impressions, ad, report)
+    def ad_to_report(available_impressions, ad, report)
       percentage = percentage_available(available_impressions, ad)
       percentage = (percentage > 100.00 || ad.goal == 0) ? 100.00 : percentage
       report.push(ad_id: ad.id, percentage: percentage)
+    end
+
+    def ads_to_report(available_impressions, ads_with_same_prio, report)
+      total_used = 0
+      total_daily_demand = ads_with_same_prio.map{|ad| daily_goal(ad) }.sum
+      if (total_daily_demand <=  available_impressions) || available_impressions == 0
+        ads_with_same_prio.each { |ad| ad_to_report(available_impressions/ads_with_same_prio.count, ad, report) }
+        total_daily_demand
+      else
+        if ads_with_same_prio.count == 1
+          ad_to_report(available_impressions, ads_with_same_prio[0], report)
+          available_impressions
+        else
+          min_assignment_amount = available_impressions / ads_with_same_prio.count
+          sorted = ads_with_same_prio.sort { |ad| daily_goal(ad) }.reverse
+          ad, *ads= sorted
+          available = (min_assignment_amount > daily_goal(ad)) ? daily_goal(ad) : min_assignment_amount
+          ad_to_report(available, ad, report)
+          available + ads_to_report(available_impressions - available, ads, report)
+        end
+      end
     end
 
     def percentage_available(available_impressions, ad)
--- spec/models/zone_ad_forecast_spec.rb
+++ spec/models/zone_ad_forecast_spec.rb
@@ -45,4 +45,47 @@ describe ZoneAdForecast do
               { ad_id: 111, percentage: 100.0 },
               { ad_id: 113, percentage: 25.0 }])
   end
+
+  it 'handles multiple forcasts with the same priority and same demand' do
+    create_ad(priority: 2, goal: 15_000, id: 113)
+    create_ad(priority: 2, goal: 15_000, id: 111)
+    create_ad(priority: 3, goal: 30000, id: 112)
+    create_ad(priority: 1, goal: 80_000, id: 114)
+    expect(described_class.forecast(zone.id, date))
+      .to eq([{ ad_id: 112, percentage: 100.00 },
+              { ad_id: 111, percentage: 33.33 },
+              { ad_id: 113, percentage: 33.33 },
+              { ad_id: 114, percentage: 0.0 }])
+  end
+
+  it 'handles multiple forcasts with the same priority and different demands' do
+    create_ad(priority: 2, goal: 5_000, id: 113)
+    create_ad(priority: 2, goal: 4_000, id: 112)
+    create_ad(priority: 2, goal: 2_500, id: 111)
+    create_ad(priority: 2, goal: 2_000, id: 110)
+    create_ad(priority: 3, goal: 30000, id: 109)
+    create_ad(priority: 1, goal: 80_000, id: 114)
+    forcast = described_class.forecast(zone.id, date)
+    expect(described_class.forecast(zone.id, date).to_set)
+      .to eq([{ ad_id: 109, percentage: 100.00},
+              { ad_id: 112, percentage: 68.75 },
+              { ad_id: 111, percentage: 100.00 },
+              { ad_id: 113, percentage: 55.00 },
+              { ad_id: 110, percentage: 100.00},
+              { ad_id: 114, percentage: 0.0 }].to_set)
+  end
+
+  it 'handles multiple forcasts with the same priority and same demands with leftover capacity' do
+    create_ad(priority: 2, goal: 4_000, id: 113)
+    create_ad(priority: 2, goal: 4_000, id: 111)
+    create_ad(priority: 3, goal: 30000, id: 112)
+    create_ad(priority: 1, goal: 80_000, id: 114)
+    expect(described_class.forecast(zone.id, date))
+      .to eq([{ ad_id: 112, percentage: 100.00 },
+              { ad_id: 111, percentage: 100.00 },
+              { ad_id: 113, percentage: 100.00 },
+              { ad_id: 114, percentage: 2.50 }])
+  end
+
+
 end
