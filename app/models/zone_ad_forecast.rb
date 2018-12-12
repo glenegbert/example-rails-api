1ee17bc206e0f43012cc96b2fa3eb84d239be6d3
Glen Egbert <glenegbert1@gmail.com>
2018-12-12 13:14:18 -0700

    same priority assignment works

--- app/models/zone_ad_forecast.rb
+++ app/models/zone_ad_forecast.rb
@@ -2,19 +2,37 @@ module ZoneAdForecast
   def self.forecast(zone_id, date)
     zone = Zone.find(zone_id)
     available_impressions = zone.impressions.to_f
-    ads(zone, date).each_with_object([]) do |ad, accum|
-      add_to_report(available_impressions, ad, accum)
-      next_impressions = available_impressions - daily_goal(ad)
+    grouped_ads = ads(zone, date).group_by(&:priority)
+    grouped_ads.values.each_with_object([]) do |ads, accum|
+      daily_assigned = ads_to_report(available_impressions, ads, accum)
+      next_impressions = available_impressions - daily_assigned
       # keep available impressions from being negative so next calculations work
       available_impressions = next_impressions.positive? ? next_impressions : 0
     end
   end
 
   class << self
-    def add_to_report(available_impressions, ad, report)
+    def ad_to_report(available_impressions, ad, report)
       percentage = percentage_available(available_impressions, ad)
-      percentage = (percentage > 100.00 || ad.goal == 0) ? 100.00 : percentage
+      percentage = percentage > 100.00 || ad.goal.zero? ? 100.00 : percentage
       report.push(ad_id: ad.id, percentage: percentage)
+    end
+
+    def ads_to_report(available_impressions, ads_with_same_priority, report)
+      if ads_with_same_priority.count == 1
+        ad = ads_with_same_priority[0]
+        available = [daily_goal(ad), available_impressions].min
+        ad_to_report(available, ad, report)
+        available
+      else
+        min_assignment_amount =
+          available_impressions / ads_with_same_priority.count
+        sorted = ads_with_same_priority.sort { |ad| daily_goal(ad) }.reverse
+        ad, *ads = sorted
+        available = [daily_goal(ad), min_assignment_amount].min
+        ad_to_report(available, ad, report)
+        available + ads_to_report(available_impressions - available, ads, report)
+      end
     end
 
     def percentage_available(available_impressions, ad)
@@ -30,8 +48,8 @@ module ZoneAdForecast
       zone
         .ads
         .where('start_date <= ? AND end_date >= ?',
-                date,
-                date)
+               date,
+               date)
         .order(priority: :desc)
     end
   end
